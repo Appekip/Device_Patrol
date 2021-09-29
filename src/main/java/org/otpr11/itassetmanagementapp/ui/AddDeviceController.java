1768a410c4a4578698f900732e04ed0f9dad6d9b
Lillinen1234 <lilli-mari.hayhanen@metropolia.fi>
2021-09-29 11:47:22 +0300

    Add choicebox for status

--- src/main/java/org/otpr11/itassetmanagementapp/ui/AddDeviceController.java
+++ src/main/java/org/otpr11/itassetmanagementapp/ui/AddDeviceController.java
@@ -11,27 +11,34 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import lombok.val;
+import org.otpr11.itassetmanagementapp.constants.DeviceStatus;
 import org.otpr11.itassetmanagementapp.constants.DeviceType;
 
 public class AddDeviceController implements Initializable {
-  ObservableList list= FXCollections.observableArrayList();
+  ObservableList list1 = FXCollections.observableArrayList();
+  ObservableList list2 = FXCollections.observableArrayList();
   @FXML
   private ChoiceBox<String> platform;
+
+  @FXML
+  private ChoiceBox<String> status;
 
   @Override
   public void initialize(URL url, ResourceBundle rb) {
     loadData();
-  }
-
-  @FXML
-  private void displayValue(ActionEvent event) {
-
+    loadStatus();
   }
 
   private void loadData(){
     val types = Arrays.stream(DeviceType.values()).map(DeviceType::toString).collect(Collectors.toList());
-    list.addAll(types);
-    platform.getItems().addAll(list);
+    list1.addAll(types);
+    platform.getItems().addAll(list1);
+  }
+
+  private void loadStatus(){
+    val types = Arrays.stream(DeviceStatus.values()).map(DeviceStatus::toString).collect(Collectors.toList());
+    list2.addAll(types);
+    status.getItems().addAll(list2);
   }
 
 }
