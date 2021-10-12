f4b0e8725e41f404ea8dee37843348f67a84e906
Linus Willner <hello@linuswillner.me>
2021-10-12 19:54:53 +0300

    Add sub-object editor controllers and boilerplate

---
+++ src/main/java/org/otpr11/itassetmanagementapp/ui/controllers/OperatingSystemEditorController.java
@@ -0,0 +1,17 @@
+package org.otpr11.itassetmanagementapp.ui.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+import lombok.Setter;
+import org.otpr11.itassetmanagementapp.Main;
+import org.otpr11.itassetmanagementapp.interfaces.ViewController;
+
+public class OperatingSystemEditorController implements Initializable, ViewController {
+  @Setter private Main main;
+  @Setter private Stage stage;
+
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {}
+}
