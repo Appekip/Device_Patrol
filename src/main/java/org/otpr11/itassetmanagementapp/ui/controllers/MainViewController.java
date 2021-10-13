fe0f517ff78ccc297b54ccb5a2b76690848d7cb8
Linus Willner <hello@linuswillner.me>
2021-10-13 11:48:33 +0300

    Add live status editing support

--- src/main/java/org/otpr11/itassetmanagementapp/ui/controllers/MainViewController.java
+++ src/main/java/org/otpr11/itassetmanagementapp/ui/controllers/MainViewController.java
@@ -11,6 +11,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.SplitMenuButton;
 import javafx.scene.control.TableCell;
@@ -27,6 +28,7 @@ import org.otpr11.itassetmanagementapp.db.core.DTO;
 import org.otpr11.itassetmanagementapp.db.core.DatabaseEventPropagator;
 import org.otpr11.itassetmanagementapp.db.dao.GlobalDAO;
 import org.otpr11.itassetmanagementapp.db.model.Device;
+import org.otpr11.itassetmanagementapp.db.model.Status;
 import org.otpr11.itassetmanagementapp.interfaces.DatabaseEventListener;
 import org.otpr11.itassetmanagementapp.interfaces.ViewController;
 import org.otpr11.itassetmanagementapp.ui.utils.CellDataFormatter;
@@ -36,10 +38,10 @@ import org.otpr11.itassetmanagementapp.utils.JFXUtils;
 @Log4j2
 public class MainViewController implements Initializable, ViewController, DatabaseEventListener {
   private final GlobalDAO dao = GlobalDAO.getInstance();
+  private final List<Status> statuses = dao.statuses.getAll();
   @Setter private Main main;
   @Setter private Stage stage;
   @Setter private Object sceneChangeData;
-
   @FXML private TableView<Device> deviceTable;
   @FXML private TableColumn<Device, String> idColumn;
   @FXML private TableColumn<Device, String> nicknameColumn;
@@ -50,7 +52,7 @@ public class MainViewController implements Initializable, ViewController, Databa
   @FXML private TableColumn<Device, String> deviceTypeColumn;
   @FXML private TableColumn<Device, String> hwConfigurationColumn;
   @FXML private TableColumn<Device, String> userColumn;
-  @FXML private TableColumn<Device, String> statusColumn;
+  @FXML private TableColumn<Device, Device> statusColumn;
   @FXML private TableColumn<Device, String> locationColumn;
   @FXML private TableColumn<Device, String> osColumn;
   @FXML private TableColumn<Device, Device> actionColumn;
@@ -72,8 +74,25 @@ public class MainViewController implements Initializable, ViewController, Databa
     hwConfigurationColumn.setCellValueFactory(CellDataFormatter::formatHWConfig);
     osColumn.setCellValueFactory(CellDataFormatter::formatOS);
     userColumn.setCellValueFactory(CellDataFormatter::formatUser);
-    statusColumn.setCellValueFactory(new PropertyValueFactory<>("status"));
+    // statusColumn.setCellValueFactory(new PropertyValueFactory<>("status"));
     locationColumn.setCellValueFactory(CellDataFormatter::formatLocation);
+
+    statusColumn.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+    statusColumn.setCellFactory(
+        param ->
+            new TableCell<>() {
+              @Override
+              protected void updateItem(Device device, boolean b) {
+                super.updateItem(device, b);
+
+                if (device == null) {
+                  setGraphic(null);
+                  return;
+                }
+
+                setGraphic(createStatusDropdown(device.getId()));
+              }
+            });
 
     actionColumn.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
     actionColumn.setCellFactory(
@@ -164,8 +183,22 @@ public class MainViewController implements Initializable, ViewController, Databa
     return button;
   }
 
+  private ChoiceBox<String> createStatusDropdown(String deviceID) {
+    val dropdown = new ChoiceBox<String>();
+    JFXUtils.select(dropdown, dao.devices.get(deviceID).getStatus());
+    statuses.forEach(status -> dropdown.getItems().add(status.toString()));
+    dropdown.setOnAction(event -> updateDeviceStatus(deviceID, dao.statuses.get(dropdown.getValue())));
+    return dropdown;
+  }
+
   private void updateItems(List<Device> devices) {
     deviceTable.setItems(FXCollections.observableArrayList(devices));
+  }
+
+  private void updateDeviceStatus(String deviceID, Status status) {
+    val device = dao.devices.get(deviceID);
+    device.setStatus(status);
+    dao.devices.save(device);
   }
 
   @Override
