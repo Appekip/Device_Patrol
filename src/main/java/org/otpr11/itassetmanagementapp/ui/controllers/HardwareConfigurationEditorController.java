cbc8a35f3a5b2a067e6d8ac04eb5b91b622956dc
Lillinen1234 <lilli-mari.hayhanen@metropolia.fi>
2021-11-04 09:17:57 +0200

    Add first version of UI for hw configuration editor

--- src/main/java/org/otpr11/itassetmanagementapp/ui/controllers/HardwareConfigurationEditorController.java
+++ src/main/java/org/otpr11/itassetmanagementapp/ui/controllers/HardwareConfigurationEditorController.java
@@ -2,10 +2,17 @@ package org.otpr11.itassetmanagementapp.ui.controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import lombok.Setter;
 import org.otpr11.itassetmanagementapp.Main;
+import org.otpr11.itassetmanagementapp.db.dao.GlobalDAO;
+import org.otpr11.itassetmanagementapp.db.model.Device;
+import org.otpr11.itassetmanagementapp.db.model.configuration.Configuration;
 import org.otpr11.itassetmanagementapp.interfaces.ViewController;
 
 public class HardwareConfigurationEditorController implements Initializable, ViewController {
@@ -13,8 +20,34 @@ public class HardwareConfigurationEditorController implements Initializable, Vie
   @Setter private Stage stage;
   @Setter private Object sceneChangeData;
 
+  private final GlobalDAO dao = GlobalDAO.getInstance();
+  private final Configuration configuration = new Configuration();
+
+  @FXML
+  private TextField configurationIDField,
+      cpuField,
+      gpuField,
+      memoryField,
+      diskSizeField,
+      screenSizeField;
+
+  @FXML
+  private Button okButton,
+      cancelButton;
+
   @Override
-  public void initialize(URL url, ResourceBundle resourceBundle) {}
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+    //okButton.setOnAction(this::onSave);
+    cancelButton.setOnAction(this::onCancel);
+  }
+
+  private void onSave(){
+    //configuration.setLaptopConfiguration();
+  }
+
+  private void onCancel(ActionEvent event) {
+    stage.close();
+  }
 
   @Override
   public void afterInitialize() {}
