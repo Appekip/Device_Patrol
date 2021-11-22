265504cbcc9f9ea9272b84f5f40efb7585ba6097
Lillinen1234 <lilli-mari.hayhanen@metropolia.fi>
2021-11-22 20:20:21 +0200

    Add validators to hw-configuration-editor

--- src/main/java/org/otpr11/itassetmanagementapp/ui/controllers/HardwareConfigurationEditorController.java
+++ src/main/java/org/otpr11/itassetmanagementapp/ui/controllers/HardwareConfigurationEditorController.java
@@ -4,17 +4,22 @@ import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
+import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import lombok.Setter;
+import lombok.extern.log4j.Log4j2;
 import lombok.val;
+import net.synedra.validatorfx.Validator;
 import org.otpr11.itassetmanagementapp.Main;
 import org.otpr11.itassetmanagementapp.constants.DeviceType;
 import org.otpr11.itassetmanagementapp.db.dao.GlobalDAO;
@@ -22,31 +27,35 @@ import org.otpr11.itassetmanagementapp.db.model.configuration.Configuration;
 import org.otpr11.itassetmanagementapp.db.model.configuration.DesktopConfiguration;
 import org.otpr11.itassetmanagementapp.db.model.configuration.LaptopConfiguration;
 import org.otpr11.itassetmanagementapp.interfaces.ViewController;
+import org.otpr11.itassetmanagementapp.utils.AlertUtils;
 
+@Log4j2
 public class HardwareConfigurationEditorController implements Initializable, ViewController {
-  @Setter private Main main;
-  @Setter private Stage stage;
-  @Setter private Object sceneChangeData;
-
   private static final DeviceType DEFAULT_DEVICE_TYPE = DeviceType.LAPTOP;
 
   private final GlobalDAO dao = GlobalDAO.getInstance();
   private final Configuration configuration = new Configuration();
   private final DesktopConfiguration desktopConfiguration = new DesktopConfiguration();
   private final LaptopConfiguration laptopConfiguration = new LaptopConfiguration();
+  private final Validator validator = new Validator();
+
 
   private final List<String> deviceTypes =
-      Arrays.stream(DeviceType.values()).map(DeviceType::toString).collect(Collectors.toList());
+        Arrays.stream(DeviceType.values()).map(DeviceType::toString).collect(Collectors.toList());
+
+  @Setter private Main main;
+  @Setter private Stage stage;
+  @Setter private Object sceneChangeData;
 
   @FXML
   private ChoiceBox<String> deviceTypeField;
 
   @FXML
   private TextField cpuField,
-      gpuField,
-      memoryField,
       diskSizeField,
-      screenSizeField;
+      screenSizeField,
+      gpuField,
+      memoryField;
 
   @FXML
   private Text screenSizeText;
@@ -58,12 +67,18 @@ public class HardwareConfigurationEditorController implements Initializable, Vie
 
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
+    createTextFieldValidator(cpuField, "cpu", cpuField.textProperty());
+    createTextFieldValidator(diskSizeField, "diskSize", diskSizeField.textProperty());
+    createTextFieldValidator(screenSizeField, "screenSize", screenSizeField.textProperty());
+    createTextFieldValidator(gpuField, "gpu", gpuField.textProperty());
+    createTextFieldValidator(memoryField, "memory", memoryField.styleProperty());
+
     initDropdown(deviceTypeField, deviceTypes, DEFAULT_DEVICE_TYPE.toString());
 
     deviceTypeField.setOnAction(
         event -> {
           val type = DeviceType.valueOf(deviceTypeField.getSelectionModel().getSelectedItem());
-          if (deviceTypeField.getValue() == "DESKTOP") {
+          if (type.toString().equals("DESKTOP")) {
             screenSizeField.setEditable(false);
             screenSizeField.setVisible(false);
             screenSizeText.setVisible(false);
@@ -84,20 +99,28 @@ public class HardwareConfigurationEditorController implements Initializable, Vie
     dropdown.setValue(initialValue);
   }
 
-  private void onSave(ActionEvent event){
+  private void onSave(ActionEvent event) {
 
-    if (deviceTypeField.getValue() == "LAPTOP"){
-      saveLaptop();
+    if (validator.containsErrors()) {
+      AlertUtils.showAlert(
+          AlertType.ERROR,
+          "Invalid input",
+          "One or more required field values are missing or invalid.");
     }
     else {
-      saveDesktop();
+      if (deviceTypeField.getValue().equals("LAPTOP")) {
+        saveLaptop();
+      }
+      else {
+        saveDesktop();
+      }
     }
 
     dao.devices.save(configuration);
     stage.close();
   }
 
-  private void saveLaptop(){
+  private void saveLaptop() {
     laptopConfiguration.setCpu(cpuField.getText());
     laptopConfiguration.setDiskSize(diskSizeField.getText());
     laptopConfiguration.setGpu(gpuField.getText());
@@ -108,7 +131,7 @@ public class HardwareConfigurationEditorController implements Initializable, Vie
     configuration.setLaptopConfiguration(laptopConfiguration);
   }
 
-  private void saveDesktop(){
+  private void saveDesktop() {
     desktopConfiguration.setCpu(cpuField.getText());
     desktopConfiguration.setDiskSize(diskSizeField.getText());
     desktopConfiguration.setGpu(gpuField.getText());
@@ -122,6 +145,35 @@ public class HardwareConfigurationEditorController implements Initializable, Vie
     stage.close();
   }
 
+
+  private void createTextFieldValidator(TextField field, String key, StringProperty prop) {
+    val edited = new AtomicBoolean(false);
+
+    validator
+        .createCheck()
+        .dependsOn(key, prop)
+        .withMethod(
+            ctx -> {
+              val warn = "Required field.";
+              val error = "Please provide a value.";
+              String value = ctx.get(key);
+
+              if (value == null || value.trim().equals("")) {
+                if (!edited.get()) { // Not yet edited, show only warning
+                  ctx.warn(warn);
+                  edited.set(true);
+                }
+                else { // Already edited, show error now
+                  ctx.error(error);
+                }
+              }
+            })
+        .decorates(field)
+        .immediate();
+  }
+
+
   @Override
-  public void afterInitialize() {}
+  public void afterInitialize() {
+  }
 }
