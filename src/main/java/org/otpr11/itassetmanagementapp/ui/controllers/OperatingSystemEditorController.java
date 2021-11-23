4f1f92caca2e11560049d5f9a18bab42deb9049c
Lillinen1234 <lilli-mari.hayhanen@metropolia.fi>
2021-11-23 14:59:28 +0200

    Fix OS-editor logic

--- src/main/java/org/otpr11/itassetmanagementapp/ui/controllers/OperatingSystemEditorController.java
+++ src/main/java/org/otpr11/itassetmanagementapp/ui/controllers/OperatingSystemEditorController.java
@@ -1,11 +1,8 @@
 package org.otpr11.itassetmanagementapp.ui.controllers;
 
 import java.net.URL;
-import java.util.Arrays;
-import java.util.List;
 import java.util.ResourceBundle;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.stream.Collectors;
 import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -18,15 +15,9 @@ import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
 import lombok.val;
 import net.synedra.validatorfx.Validator;
-import org.controlsfx.control.CheckComboBox;
 import org.otpr11.itassetmanagementapp.Main;
-import org.otpr11.itassetmanagementapp.constants.DeviceStatus;
-import org.otpr11.itassetmanagementapp.constants.DeviceType;
 import org.otpr11.itassetmanagementapp.db.dao.GlobalDAO;
-import org.otpr11.itassetmanagementapp.db.model.Device;
 import org.otpr11.itassetmanagementapp.db.model.OperatingSystem;
-import org.otpr11.itassetmanagementapp.db.model.Status;
-import org.otpr11.itassetmanagementapp.db.model.configuration.Configuration;
 import org.otpr11.itassetmanagementapp.interfaces.ViewController;
 import org.otpr11.itassetmanagementapp.utils.AlertUtils;
 
@@ -42,15 +33,7 @@ public class OperatingSystemEditorController implements Initializable, ViewContr
 
   private final GlobalDAO dao = GlobalDAO.getInstance();
   private final OperatingSystem operatingSystem = new OperatingSystem();
-  private final Configuration configuration = new Configuration();
-  private final Status status = new Status();
   private final Validator validator = new Validator();
-
-  private static final DeviceType DEFAULT_DEVICE_TYPE = DeviceType.LAPTOP;
-  private static final DeviceStatus DEFAULT_DEVICE_STATUS = DeviceStatus.VACANT;
-  private static final String OS_SELECTOR_DEFAULT_TILE = "Select...";
-  private static boolean IS_EDIT_MODE;
-
 
   @FXML
   private TextField nameField,
@@ -60,8 +43,6 @@ public class OperatingSystemEditorController implements Initializable, ViewContr
   @FXML
   private Button okButton,
     cancelButton;
-
-  @FXML private CheckComboBox<String> osSelector;
 
   @Override
   public void initialize(URL url, ResourceBundle rb) {
@@ -74,24 +55,20 @@ public class OperatingSystemEditorController implements Initializable, ViewContr
   }
 
   private void onSave(ActionEvent event) {
-    if (validator.containsErrors()) {
+    if (validator.containsErrors() || validator.containsWarnings()) {
       AlertUtils.showAlert(
           AlertType.ERROR,
           "Invalid input",
           "One or more required field values are missing or invalid.");
-    } else if (osSelector.getCheckModel().getCheckedItems().size() == 0) {
-      AlertUtils.showAlert(
-          AlertType.ERROR,
-          "No operating system selected",
-          "No operating system has been selected for this device.");
-    } else if (!IS_EDIT_MODE && dao.devices.get(nameField.getText()) != null) {
-      AlertUtils.showAlert(
-          AlertType.ERROR, "Duplicate ID detected", "A device with this ID already exists.");
-    } else {
+    }
+    else {
       // Set basic properties
       operatingSystem.setName(nameField.getText());
       operatingSystem.setBuildNumber(buildNumberField.getText());
       operatingSystem.setVersion(versionField.getText());
+
+      dao.devices.save(operatingSystem);
+      stage.close();
 
     }
   }
